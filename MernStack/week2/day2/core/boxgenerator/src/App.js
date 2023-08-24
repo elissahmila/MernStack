52467d1b99f40cef6d956cfa89db89a4e562271e
elissahmila <139132211+elissahmila@users.noreply.github.com>
2023-08-24 19:41:30 +0100

    boxgenerator

---
+++ MernStack/week2/day2/core/boxgenerator/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import  Box  from './components/Box';
+function App() {
+  const table=[]
+  return (
+    <div className="App">
+     < Box theTable={table} />
+    </div>
+  );
+}
+
+export default App;
---
+++ MernStack/week2/day2/core/boxgenerator/src/components/Box.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+export const Box = (props) => {
+    const [ Box, setBoxes] = useState(props.theTable);
+    const [ color, setColor] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setBoxes([...Box,color])  
+    }
+  return (
+    
+    <div>
+    <div>
+    {
+     Box.map( (theTable)=>{
+            return <div style={{
+            display:'inline-block',    
+            height:'80px',
+            width:'80px',
+            backgroundColor:theTable
+            }}></div>
+     } )
+
+    }    
+        
+    </div>    
+    <form onSubmit={handleSubmit}>
+        <div className='myform'>
+        <p>Color</p> <input type="text" onChange={(e)=>{setColor(e.target.value)}} /> 
+       <button>add</button>
+       </div>
+       </form>
+       </div>
+  )
+}
+export default Box
